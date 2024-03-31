251e64ff3eed9e88a715cb1486151699a2bcd40e
parinrava <pravanbakhsh@my.bcit.ca>
2024-03-30 23:31:47 -0700

    fixed navabr

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #E9EDC9 !important;">
-        <div class="container-fluid">
+    <div class="container-fluid">
         <a class="navbar-brand" href="#">Laravel Expense Manager</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('transactions.index') }}">Transactions</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/buckets">Buckets</a>
+                    <a class="nav-link" href="/buckets"> Buckets </a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Reports
+                         Reports  
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <li class="px-3 py-1">
@@ -39,13 +39,14 @@
                 <li class="nav-item">
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
-                        <button type="submit" class="nav-link btn btn-link" style="display: block; padding: 0.5rem 1rem; margin: 0; border: none; background: none;">Logout</button>
+                        <button type="submit" class="nav-link btn btn-link" style="display: block; padding: 0.5rem 1rem; margin: 0; border: none; background: none;"> Logout</button>
                     </form>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
 
 <script>
     function navigateToYearReport() {
@@ -55,3 +56,9 @@
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  
+
+  
+  
+  
